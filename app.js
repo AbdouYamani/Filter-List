6fb37d49d32263fbcb4f93d063060c796029b33d
unknown <pikatureetaa@gmail.com>
2021-02-24 23:54:51 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+let Filterinput = document.getElementById("filterInput");
+Filterinput.addEventListener('keyup',filterNames);
+
+ function filterNames ()
+ {
+  let filterValue = document.getElementById('filterInput').value.toUpperCase();
+  console.log(filterValue);
+  let ul = document.getElementById('names');
+  let li = ul.querySelectorAll('li.collection-item');
+  for (let i = 0 ; i < li.length ; i++)
+  {
+    let a = li[i].getElementsByTagName('a')[0];
+    if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1)
+    {
+      li[i].style.display = '';
+    }else{
+      li[i].style.display = 'none';
+    }
+  }
+
+ }
+
+
+ 
